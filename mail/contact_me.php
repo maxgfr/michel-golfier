2cbffb6198cb097c3297f9b79d0fea36318cea2f
Maxime Golfier <maxgolfier@gmail.com>
2017-12-12 15:42:30 +0100

    version 1.5

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -4,20 +4,20 @@
 	   		echo "Element non indiqué!";
 	   		return false;
 	}
-	   
+
 	$name = strip_tags(htmlspecialchars($_POST['name']));
 	$email_address = strip_tags(htmlspecialchars($_POST['email']));
 	$phone = strip_tags(htmlspecialchars($_POST['phone']));
 	$message = strip_tags(htmlspecialchars($_POST['message']));
-	   
+
 	// Create the email and send the message
 	$to = 'michelgolfier@sfr.fr'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 	$email_subject = "Formulaire de contact:  $name";
 	$email_body = "Tu as reçu un nouveau message.\n\n"."Voici les détails:\n\nNom: $name\n\nEmail: $email_address\n\nNuméro: $phone\n\nMessage:\n$message";
-	$headers = "From: noreply@creatb.gq\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+	$headers = "From: noreply@michel-golfier.fr\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 	$headers .= "Reply-To: $email_address"; 
 
 	mail($to,$email_subject,$email_body,$headers);
 
-	return true;         
+	return true;
 ?>
