fa0b39a6df9c06e2e6ab309fa74d4f1c96f0e544
maxgfr <25312957+maxgfr@users.noreply.github.com>
2021-11-28 15:20:10 +0100

    fix: version

--- pages/ouvrages/[book].tsx
+++ pages/ouvrages/[book].tsx
@@ -3,6 +3,14 @@ import { useState } from "react";
 import { Document, Page as Pdf, pdfjs } from "react-pdf";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
+const Neschers = {
+  path: "/assets/L-Histoire-de-Neschers.pdf",
+};
+
+const JbCroizet = {
+  path: "/assets/Jean-Baptiste-Croizet.pdf",
+};
+
 const Page: NextPage = () => {
   const [numPages, setNumPages] = useState(null);
   const [pageNumber, setPageNumber] = useState(3);
@@ -13,10 +21,7 @@ const Page: NextPage = () => {
 
   return (
     <>
-      <Document
-        file="/assets/L-Histoire-de-Neschers.pdf"
-        onLoadSuccess={onDocumentLoadSuccess}
-      >
+      <Document file={JbCroizet.path} onLoadSuccess={onDocumentLoadSuccess}>
         <Pdf pageNumber={pageNumber} />
       </Document>
       <p>
