6fb48dbb53ab208bb066c95d636fc80d955043e2
maxgfr <25312957+maxgfr@users.noreply.github.com>
2023-01-19 10:54:46 +0100

    fix: csp config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,29 @@
+const ContentSecurityPolicy = `
+  default-src 'self';
+  img-src 'self' data:;
+  script-src 'self' cdnjs.cloudflare.com ${
+    process.env.NODE_ENV !== "production" && "'unsafe-eval'"
+  };
+  style-src 'self' 'unsafe-inline';
+  font-src 'self' data:;
+  worker-src 'self' blob: cdnjs.cloudflare.com;
+`;
+
 /** @type {import('next').NextConfig} */
 module.exports = {
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: ContentSecurityPolicy.replace(/\n/g, " ").trim(),
+          },
+        ],
+      },
+    ];
+  },
   reactStrictMode: true,
   serverRuntimeConfig: {
     PROJECT_ROOT: __dirname,
