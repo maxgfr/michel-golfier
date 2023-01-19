9ccf5f72f988666722e3a7f5f694f34ed0ac2af1
maxgfr <25312957+maxgfr@users.noreply.github.com>
2023-01-19 12:09:21 +0100

    fix: csp config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 const ContentSecurityPolicy = `
   default-src 'self';
   img-src 'self' data:;
-  script-src 'self' ${process.env.NODE_ENV !== "production" && "'unsafe-eval'"};
+  script-src 'self' 'unsafe-eval';
   style-src 'self' 'unsafe-inline';
   font-src 'self' data:;
   connect-src 'self' vitals.vercel-insights.com;
